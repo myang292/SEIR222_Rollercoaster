b5184ba4c15cdc0383e57bc6da2e5dfac044868a
Michelle Yang <myang292@gmail.com>
2022-03-24 08:12:25 -0700

    Update models

--- BackEnd/models/park.js
+++ BackEnd/models/park.js
@@ -1,11 +1,13 @@
-const { Schema } = require('')
+const { Schema } = require('mongoose')
 
 const Parks = new Schema(
     {
-        park: { type: String, required: true },
+        name: { type: String, required: true },
+        description: { type: String, required: true },
         location: { type: String, required: true },
-        address: {type: String, required: true },
-        rides: { type: String, required: true }
+        address: { type: String, required: true },
+        url: { type: String, required: true },
+        img: { type: String, required: true },
     },
     {timestamps: true}
 )
--- BackEnd/models/ride.js
+++ BackEnd/models/ride.js
@@ -1,11 +1,16 @@
+const { ObjectId } = require('bson')
 const { Schema } = require('mongoose')
 
 const Rides = new Schema (
     {
         name: { type: String, required: true},
-        length: {type: String, required: true},
-        top_speed: {type: String, required: true},
-        height: {type: String, required: true}
+        park: { type: ObjectId, required: true},
+        description: { type: String, required: true },
+        height: { type: Number, required: true },
+        length: { type: Number, required: true },
+        speed: { type: Number, required: true },
+        type: { type: String, required: true },
+        img: { type: String, required: true }
     }, 
     {timestamps: true}
 )
